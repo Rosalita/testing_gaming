bd2cfe3082429a3f13c7086157c04f3fddbc7b71
Rosie Hamilton <rhamilton@scottlogic.co.uk>
2017-11-16 16:57:51 +0000

    initial commit

---
+++ analysis.r
@@ -0,0 +1,11 @@
+
+#import ggplot2
+library(ggplot2)
+
+# Set working dir
+setwd ("/Dev/Git/testing_gaming")
+#setwd("~/git/tester_survey")
+
+# Read in data
+mydata <- read.csv("data.csv", header = TRUE, sep =",")
+
